7c4c39ca3b0328a1d2930a7bdd608c33146074a4
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2024-09-22 22:43:19 +1000

    add in averages

--- AzureDailyCostCompare/ReportGenerator.cs
+++ AzureDailyCostCompare/ReportGenerator.cs
@@ -7,6 +7,9 @@ class ReportGenerator
     private DateHelperService dateHelperService;
     private List<DailyCosts> currentMonthCostData;
     private List<DailyCosts> previousMonthCostData;
+    private decimal averageCurrentMonth;
+    private decimal averageLastMonth;
+
 
     public ReportGenerator(List<DailyCosts> costData, DateHelperService dateHelperService)
     {
@@ -19,6 +22,12 @@ class ReportGenerator
         previousMonthCostData = costData
                 .Where(dc => dc.DateString.Month == dateHelperService.FirstDayOfPreviousMonth.Month && dc.DateString.Year == dateHelperService.FirstDayOfPreviousMonth.Year)
                 .ToList();
+
+        averageCurrentMonth = currentMonthCostData // dont include the last day as the data is not complete and skews the average down
+            .Take(currentMonthCostData.Count - 1)
+            .Average(dc => dc.Cost);
+        averageLastMonth = previousMonthCostData.Average(dc => dc.Cost);
+
     }
     public void GenerateDailyCostReport()
     {
@@ -47,5 +56,10 @@ class ReportGenerator
 
             Console.WriteLine("{0,-18} {1,-18} {2,-18} {3,-18}", row.DayOfMonth, previousCost, currentCost, costDifference);
         }
+
+
+        Console.WriteLine("\nDaily Averages in USD:");
+        Console.WriteLine("Current Month Average(not inlcuding current day as data is incomplete): {0:F2}", averageCurrentMonth);
+        Console.WriteLine("Last Month Average: {0:F2}", averageLastMonth);
     }
 }
