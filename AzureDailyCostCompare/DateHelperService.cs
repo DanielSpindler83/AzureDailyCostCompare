b38d9ad619ddac9ffca2c5dab405ba98a65f46a0
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2024-12-15 20:56:32 +1000

    fix logic bug with referencedate time

--- AzureDailyCostCompare/DateHelperService.cs
+++ AzureDailyCostCompare/DateHelperService.cs
@@ -19,12 +19,15 @@ public class DateHelperService
         // Get the reference datetime
         var referenceDateTime = overrideDate ?? provider.GetCurrentDate();
 
-        // Determine the data reference date, accounting for the 6am UTC cutoff to ensure a complete day data set
-        DataReferenceDate = referenceDateTime.Hour < 6
-            ? referenceDateTime.Date.AddDays(-1)  // Before 6am UTC, use previous day
-            : referenceDateTime.Date;
         // NOTE if we set historical date to be 1st UTC with midnight time(the default) - do we have a bug? What does it show? last 2 months without last day of month....not ideal
         // For historical if we want to see two full months(all days complete) - set datetime between 1st of the month 6am and 2nd of the month 5:59am
+
+        // NOTE 6am is a educated guess regarding MS Cost API having the full days data mostly done and available
+        // Determine the data reference date, accounting for the 6am UTC cutoff to ensure a complete day data set
+        // Once we pass 6am UTC - the full cost data is available for the date prior to todays UTC date
+        DataReferenceDate = referenceDateTime.Hour < 6
+            ? referenceDateTime.Date.AddDays(-2)  // Before 6am UTC, we dont have full day today so last full day is 2 days ago
+            : referenceDateTime.Date.AddDays(-1); // its after 6am UTC so yesterday is last full day
 
         FirstDayOfPreviousMonth = new DateTime(DataReferenceDate.Year, DataReferenceDate.Month, 1).AddMonths(-1);
         FirstDayOfCurrentMonth = new DateTime(DataReferenceDate.Year, DataReferenceDate.Month, 1);
