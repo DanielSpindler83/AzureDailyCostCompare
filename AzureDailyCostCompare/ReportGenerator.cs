5b7e8385476edad3834452611c9999264e3e9954
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2024-12-17 20:55:54 +1000

    fix up header

--- AzureDailyCostCompare/ReportGenerator.cs
+++ AzureDailyCostCompare/ReportGenerator.cs
@@ -127,10 +127,8 @@ class ReportGenerator
 
     private void PrintMonthlyAveragesTable()
     {
-        PrintSectionHeader("Monthly Cost Averages");
-
         // Table header
-        Console.WriteLine("{0,-60} {1,10}", "", "Amount (USD)");
+        Console.WriteLine("{0,-60} {1,10}", "Monthly Cost Averages", "Amount (USD)");
         Console.WriteLine(new string('-', 72));
 
         // Current month partial average
