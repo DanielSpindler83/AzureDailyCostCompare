328404fa8df959724e425a75868821e3cccbaf17
Daniel Spindler <30247817+DanielSpindler83@users.noreply.github.com>
2025-06-07 17:38:10 +1000

    remove redundant methods in Month Calculation Service and clean up other commented out code no longer needed.

--- AzureDailyCostCompare/Application/MonthCalculationService.cs
+++ AzureDailyCostCompare/Application/MonthCalculationService.cs
@@ -1,38 +1,14 @@
 ﻿namespace AzureDailyCostCompare.Application;
 
-
-/// <summary>
-/// Calculates month-related date information
-/// APPLICATION: Service for month boundary and day count calculations
-/// </summary>
 public class MonthCalculationService
 {
-    /// <summary>
-    /// Gets the first day of the month for a given date
-    /// </summary>
-    public DateTime GetFirstDayOfMonth(DateTime date)
-    {
-        return new DateTime(date.Year, date.Month, 1);
-    }
-
-    /// <summary>
-    /// Gets the number of days in a month for a given date
-    /// </summary>
-    public int GetDaysInMonth(DateTime date)
-    {
-        return DateTime.DaysInMonth(date.Year, date.Month);
-    }
-
-    /// <summary>
-    /// Calculates month boundaries and day counts for a reference date
-    /// </summary>
     public (DateTime CurrentStart, DateTime PreviousStart, int CurrentDays, int PreviousDays)
         CalculateMonthBoundaries(DateTime referenceDate)
     {
-        var currentStart = GetFirstDayOfMonth(referenceDate);
+        var currentStart = new DateTime(referenceDate.Year, referenceDate.Month, 1);
         var previousStart = currentStart.AddMonths(-1);
-        var currentDays = GetDaysInMonth(referenceDate);
-        var previousDays = GetDaysInMonth(previousStart);
+        var currentDays = DateTime.DaysInMonth(referenceDate.Year, referenceDate.Month);
+        var previousDays = DateTime.DaysInMonth(previousStart.Year, previousStart.Month);
 
         return (currentStart, previousStart, currentDays, previousDays);
     }
